6aa6a924e9f7eceda4079203130d44e6ac623316
casual-karma-e108 <akravecas@yahoo.com>
2021-02-16 23:47:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  [4,5,6,"hello"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.unshift
 end
 
 def add_element_to_start_of_array(array, element)
