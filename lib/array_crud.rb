39a9b6b6f798c4646b12082087f179e217fd6d70
casual-karma-e108 <akravecas@yahoo.com>
2021-02-16 23:52:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "hell"
+  add_element_to_end_of_array =[4,5,6]
+  add_element_to_end_of_array << "hell"
 end
 
 def add_element_to_start_of_array(array, element)
